ce6fb5d0ae11084381c7d509eec0b60bd76025f5
Jingyu Lee <dostos10@gmail.com>
2020-09-13 19:25:22 +0900

    Update build script to generate gpu lib

--- build_aar_armv8.sh
+++ build_aar_armv8.sh
@@ -1,2 +1,3 @@
-bazel build -c opt --verbose_failures --fat_apk_cpu=arm64-v8a   --host_crosstool_top=@bazel_tools//tools/cpp:toolchain   //tensorflow/lite/java:tensorflow-lite
+bazel build -c opt --jobs=8 --verbose_failures --fat_apk_cpu=arm64-v8a   --host_crosstool_top=@bazel_tools//tools/cpp:toolchain   //tensorflow/lite/java:tensorflow-lite //tensorflow/lite/java:tensorflow-lite-gpu
 cp bazel-bin/tensorflow/lite/java/tensorflow-lite.aar .
+cp bazel-bin/tensorflow/lite/java/tensorflow-lite-gpu.aar .
