e0891f917bac347ca5d352244c1f9c90ca4cea8c
Jingyu Lee <dostos10@gmail.com>
2020-10-06 10:46:40 +0900

    fix

--- tensorflow/lite/tools/logging_reporter.cc
+++ tensorflow/lite/tools/logging_reporter.cc
@@ -1,8 +1,6 @@
 #include "tensorflow/lite/tools/logging_reporter.h"
 #include "tensorflow/lite/tools/logging.h"
 
-#include <memory>
-
 namespace tflite {
 int LoggingReporter::Report(const char* format, va_list args) {
   int count = snprintf(nullptr, 0, format, args) + 1;
@@ -14,7 +12,7 @@ int LoggingReporter::Report(const char* format, va_list args) {
 }
 
 LoggingReporter* LoggingReporter::DefaultLoggingReporter() {
-  static std::unique_ptr<LoggingReporter> error_reporter = std::make_unique<LoggingReporter>();
-  return error_reporter.get();
+  static LoggingReporter error_reporter = LoggingReporter();
+  return &error_reporter;
 }
 }
