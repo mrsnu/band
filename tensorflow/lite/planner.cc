2b2e6c4818feba9f4e4987ca686249e8784dd58c
JeonChangMin <imsameperson@gmail.com>
2020-12-24 08:14:06 +0000

    wip

--- tensorflow/lite/planner.cc
+++ tensorflow/lite/planner.cc
@@ -5,9 +5,9 @@
 namespace tflite {
 namespace impl {
 
-Planner::Planner(Interpreter* interpreter, bool needs_profile_) {
+Planner::Planner(Interpreter* interpreter, bool needs_profile) {
   interpreter_ = interpreter;
-  needs_profile_ = needs_profile_;
+  needs_profile_ = needs_profile;
 
   // open file to write per-request timestamps later
   // TODO: make the file path a configurable command line arg
