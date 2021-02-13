3236b6157ed2c95559fad8c43549ffb9fa585e42
kdh0102 <donghyunkim816@gmail.com>
2021-02-13 18:13:17 +0900

    auto commit for experiment

--- tensorflow/lite/interpreter.cc
+++ tensorflow/lite/interpreter.cc
@@ -602,7 +602,7 @@ void Interpreter::Profile(const int num_warm_ups, const int num_runs) {
 
   for (int i = 0; i < subgraphs_size(); ++i) {
     Subgraph* subgraph = subgraphs_[i].get();
-    if (subgraph->GetKey().device_flag != kTfLiteCPU)
+    if (subgraph->GetKey().device_flag != kTfLiteGPU)
       continue;
 
     for (int i = 0; i < num_warm_ups; i++) {
