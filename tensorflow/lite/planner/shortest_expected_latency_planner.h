6bc3835be0b13f5735ad3460de4c403ed9976a13
Changjin Jeong <create.jeong@snu.ac.kr>
2021-04-09 12:15:26 +0900

    [Schedule] Add schedule_window_size control logic
    [Work]
     - Add "schedule_window_size" in RuntimeConfig
     - Add parse it from json and set it in interpreter
     - Interpreter uses window_size on creating SEL planner
     - SEL uses it as the number of jobs received from requests

--- tensorflow/lite/planner/shortest_expected_latency_planner.h
+++ tensorflow/lite/planner/shortest_expected_latency_planner.h
@@ -9,12 +9,21 @@ namespace impl {
 
 class ShortestExpectedLatencyPlanner : public Planner {
  public:
-  explicit ShortestExpectedLatencyPlanner(Interpreter* interpreter)
+  explicit ShortestExpectedLatencyPlanner(Interpreter* interpreter,
+                                          int schedule_window_size)
       : Planner(interpreter) {
     planner_thread_ = std::thread([this]{this->Plan();});
+    schedule_window_size_ = schedule_window_size;
   }
   void Plan() override;
   bool NeedProfile() override;
+
+  int GetWindowSize() {
+    return schedule_window_size_;
+  }
+
+  private:
+    int schedule_window_size_ = INT_MAX;
 };
 
 }  // namespace impl
