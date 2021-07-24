78634c00f6c5d04db086ae2dc16670c92309e093
Jingyu Lee <dostos10@gmail.com>
2021-07-24 15:13:51 +0000

    update

--- tensorflow/lite/interpreter.cc
+++ tensorflow/lite/interpreter.cc
@@ -1238,7 +1238,7 @@ Interpreter::GetShortestSubgraphIndex(std::vector<int> subgraph_indices,
     SubgraphKey& key = subgraph(subgraph_index)->GetKey();
 
     int64_t waiting_time = device_waiting[key.device_flag];
-    int64_t profiled = GetProfiledLatency(key);
+    int64_t profiled = GetExpectedLatency(key);
     int64_t expected_latency = profiled + std::max(waiting_time, start_time);
 
     if (min_latency > expected_latency) {
