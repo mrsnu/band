2f4095f2a50d382b87e47d3265a9d8e62c763976
Jingyu Lee <dostos10@gmail.com>
2021-07-26 10:20:46 +0000

    git refactor based on utility functions

---
+++ tensorflow/lite/processors/util.h
@@ -0,0 +1,17 @@
+#ifndef TENSORFLOW_LITE_PROCESSORS_UTIL_H_
+#define TENSORFLOW_LITE_PROCESSORS_UTIL_H_
+
+#include <string>
+#include <vector>
+
+namespace tflite {
+namespace impl {
+
+int TryReadInt(std::vector<std::string> paths);
+std::vector<int> TryReadInts(std::vector<std::string> paths);
+std::string TryReadString(std::vector<std::string> paths);
+
+}  // namespace impl
+}  // namespace tflite
+
+#endif  // TENSORFLOW_LITE_PROCESSORS_UTIL_H_
