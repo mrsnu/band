bf8f479d01d4bee2dadccfc9e967695a20d2abbd
Jingyu Lee <dostos10@gmail.com>
2021-08-05 22:28:14 +0900

    Update tensorflow/lite/util.h
    Co-authored-by: Changmin Jeon <12844401+JeonChangMin@users.noreply.github.com>

--- tensorflow/lite/util.h
+++ tensorflow/lite/util.h
@@ -119,13 +119,13 @@ struct Job {
   JobStatus status = kTfLiteJobQueued;
   int subgraph_idx = -1;
   int device_id = -1;
-  int64_t start_scaling_frequency = 0;
   int64_t start_frequency = 0;
+  int64_t start_scaling_frequency = 0;
   int64_t start_scaling_min_frequency = 0;
   int64_t start_scaling_max_frequency = 0;
 
-  int64_t end_scaling_frequency = 0;
   int64_t end_frequency = 0;
+  int64_t end_scaling_frequency = 0;
   int64_t end_scaling_min_frequency = 0;
   int64_t end_scaling_max_frequency = 0;
 
