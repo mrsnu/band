effedc82805017ee09ef4f73d7b5a9ed34b41426
Donghyun Kim <donghyunkim816@gmail.com>
2021-08-10 12:37:56 +0900

    move interp init

--- tensorflow/lite/interpreter.cc
+++ tensorflow/lite/interpreter.cc
@@ -140,6 +140,11 @@ Interpreter::Interpreter(ErrorReporter* error_reporter,
     error_reporter_->Report("Planner::Init() failed.");
     exit(-1);
   }
+  // Initialize configurations.
+  if (Init(runtime_config.interpreter_config) != kTfLiteOk) {
+    error_reporter_->Report("Interpreter::Init() failed.");
+    exit(-1);
+  }
 
   // Initialize configurations.
   if (Init(runtime_config.interpreter_config) != kTfLiteOk) {
