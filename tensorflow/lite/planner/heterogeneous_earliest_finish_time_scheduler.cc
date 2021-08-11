8e4fa5d306f2d70c80e006706738bb9e34fbf979
Jason (Joo Seong) Jeong <culex496782@gmail.com>
2021-08-11 20:22:10 +0900

    heft scheduler

---
+++ tensorflow/lite/planner/heterogeneous_earliest_finish_time_scheduler.cc
@@ -0,0 +1,109 @@
+#include "tensorflow/lite/planner/heterogeneous_earliest_finish_time_scheduler.h"
+
+namespace tflite {
+namespace impl {
+
+void HeterogeneousEarliestFinishTimeScheduler::Schedule(JobQueue& requests) {
+  std::set<TfLiteDeviceFlags> idle_devices = planner_->GetIdleDevices();
+  DeviceWaitingTime device_waiting = GetDeviceWaitingTime();
+  int window_size = std::min(planner_->GetWindowSize(), (int)requests.size());
+
+  // stop if there are no idle devices OR there's nothing in `requests`
+  while (!idle_devices.empty() && window_size > 0) {
+    // Lookup table for GetShortestLatency().
+    // Although it is tempting to maintain a scheduler-wide cache,
+    // the values in device_waiting are (generally) different for every while
+    // loop iteration so this cache is valid only for this specific loop iter.
+    //
+    // Although this cache looks the same as the one inside
+    // GetShortestLatency(), we can get slightly more cache hits by having
+    // it again here. The reason is because we know that `device_waiting`
+    // values are unchanged within this loop iter, so if two Jobs have the
+    // same model_id and resolved_tensors then GetShortestLatency() will
+    // return the same value. In contrast, GetShortestLatency() always skips
+    // the cache lookup if start_time is not larger than all `device_waiting`
+    // values, even if the same `device_waiting` values are given.
+    std::unordered_map<std::pair<int, std::set<int>>,
+                       std::pair<int, int64_t>,
+                       Interpreter::PairHash> cache;
+
+    // basically the same as ShortestExpectedLatencyScheduler
+    int64_t largest_shortest_latency = -1;
+    int target_job_idx = -1;
+    int target_subgraph_idx = -1;
+
+    // only check up to `window_size` requests
+    for (auto it = requests.begin(); it != requests.begin() + window_size; ++it) {
+      Job& job = *it;
+      int preceded_subgraph_index = job.previous_subgraph_indices.empty() ?
+                                    -1 : job.previous_subgraph_indices.back();
+
+      std::pair<int, int64_t> best_subgraph = {-1, INT_MAX};
+      std::pair<int, std::set<int>> cache_key = {job.model_id,
+                                                 job.resolved_tensors};
+
+      auto cache_it = cache.find(cache_key);
+      if (cache_it != cache.end()) {
+        // used cached value instead of calling GetShortestLatency()
+        best_subgraph = cache_it->second;
+      } else {
+        best_subgraph = GetInterpreter()->GetShortestLatency(
+            job.model_id, job.resolved_tensors, 0, device_waiting,
+            preceded_subgraph_index);
+
+        // insert new value into cache
+        cache[cache_key] = best_subgraph;
+      }
+
+
+      if (largest_shortest_latency < best_subgraph.second) {
+        Subgraph* target_subgraph = GetInterpreter()->subgraph(best_subgraph.first);
+        TfLiteDeviceFlags device = target_subgraph->GetKey().device_flag;
+
+        // skip this job if we can't schedule it immediately,
+        // even if this job is the "most urgent" one
+        if (idle_devices.find(device) == idle_devices.end()) {
+          continue;
+        }
+
+        largest_shortest_latency = best_subgraph.second;
+        target_job_idx = it - requests.begin();
+        target_subgraph_idx = best_subgraph.first;
+      }
+    }
+
+    if (target_job_idx < 0) {
+      // no one wants to be scheduled..
+      break;
+    }
+
+    auto requests_it = requests.begin() + target_job_idx;
+    Job job = *requests_it;
+
+    // erase the job from requests and decrement window_size
+    requests.erase(requests_it);
+    window_size--;
+
+    Subgraph* target_subgraph = GetInterpreter()->subgraph(target_subgraph_idx);
+    TfLiteDeviceFlags device = target_subgraph->GetKey().device_flag;
+    auto device_it = idle_devices.find(device);
+    assert(device_it != idle_devices.end());
+
+    // mark this device as busy
+    idle_devices.erase(device_it);
+
+    device_waiting[device] +=
+        GetInterpreter()->GetExpectedLatency(target_subgraph->GetKey());
+
+    // Update Job status specific to this planner.
+    // Common status will be updated by `EnqueueAction`.
+    if (target_subgraph->IsStart()) {
+      // only set these fields if this is the first subgraph of this model
+      job.expected_latency = largest_shortest_latency;
+    }
+    EnqueueAction(job, target_subgraph);
+  }
+}
+
+}  // namespace impl
+}  // namespace tflite
