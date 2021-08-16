e770b0e6d339f840278c695afc9e65649b11aae1
Jingyu Lee <dostos10@gmail.com>
2021-08-16 16:20:49 +0000

    fix

--- tensorflow/lite/planner/fixed_device_scheduler.cc
+++ tensorflow/lite/planner/fixed_device_scheduler.cc
@@ -11,7 +11,8 @@ void FixedDeviceScheduler::Schedule(JobQueue& requests) {
 
     int model_id = to_execute.model_id;
     int worker_id;
-    if (0 <= worker_id && worker_id < GetInterpreter()->GetNumWorkers()) {
+    if (0 <= to_execute.worker_id &&
+        to_execute.worker_id < GetInterpreter()->GetNumWorkers()) {
       worker_id = to_execute.worker_id;
     } else {
       worker_id = planner_->GetModelWorkerMap()[model_id];
