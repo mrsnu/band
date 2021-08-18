4e1f363b9eddd4380eeb9dcd7cc26361e1ad5cca
Donghyun Kim <donghyunkim816@gmail.com>
2021-08-19 01:53:28 +0900

    revert update profile

--- tensorflow/lite/interpreter.cc
+++ tensorflow/lite/interpreter.cc
@@ -272,8 +272,8 @@ Interpreter::~Interpreter() {
   }
 
   // update the profile file to include all new profile results from this run
-  //profiling::util::UpdateDatabase(profile_database_, model_configs_,
-  //                                profile_database_json_);
+  profiling::util::UpdateDatabase(profile_database_, model_configs_,
+                                  profile_database_json_);
   WriteJsonObjectToFile(profile_database_json_, profile_data_path_);
 }
 
