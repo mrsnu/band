485b0e75cc2037f8e98559a494984c0d87418735
Hyunsoo Kim <hscornelia@snu.ac.kr>
2022-10-03 12:36:02 +0900

    Implement fixed device scheduler

--- band/scheduler/fixed_device_scheduler.cc
+++ band/scheduler/fixed_device_scheduler.cc
@@ -10,29 +10,21 @@ ScheduleAction FixedDeviceScheduler::Schedule(const Context& context,
   // TODO: fallback subgraphs for FixedDevicePlanner?
   while (!requests.empty()) {
     Job to_execute = requests.front();
-    requests.pop_front();
+    requests.pop_front(); // erase job
 
     int model_id = to_execute.model_id;
-    int worker_id = to_execute.subgraph_key.GetWorkerId();
+    ModelConfig model_config = *context.GetModelConfig(model_id);
+    int device_id = model_config.device;
 
-    BAND_NOT_IMPLEMENTED;
-
-    // if (0 <= to_execute.device_id && to_execute.device_id < kBandNumDevices)
-    // {
-    //   BandDeviceFlags device_flag =
-    //       static_cast<BandDeviceFlags>(to_execute.device_id);
-    //   // TODO: Select any available device worker
-    //   BAND_REPORT_ERROR(DefaultErrorReporter(), "NOT IMPLEMENTED");
-    //   continue;
-    // } else {
-    //   // TODO: Support model assignment for fixed device scheduler
-    //   // worker_id = planner_->GetModelWorkerMap()[model_id];
-    //   BAND_REPORT_ERROR(DefaultErrorReporter(), "NOT IMPLEMENTED");
-    //   continue;
-    // }
-
-    SubgraphKey key = context.GetModelSubgraphKey(model_id, worker_id);
-    action[worker_id].push_back({to_execute, key});
+    if (0 <= device_id && device_id < kBandNumDevices)
+    {
+      WorkerId worker_id = context.GetModelWorker(model_id);
+      SubgraphKey key = context.GetModelSubgraphKey(model_id, worker_id);
+      action[worker_id].push_back({to_execute, key});
+    } else {
+      BAND_REPORT_ERROR(DefaultErrorReporter(), "Invalid device for model %s", 
+          model_config.model_fname);
+    }
   }
   return action;
 }
