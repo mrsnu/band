0a06432471906ff4f2f2308ddbd943484e3bda7e
Jingyu Lee <dostos10@gmail.com>
2022-10-13 10:30:08 +0000

    cpu fix + tested on android64 (pixel4)

---
+++ band/test/cpu_test.cc
@@ -0,0 +1,62 @@
+
+#include "band/cpu.h"
+
+#include <gtest/gtest.h>
+
+namespace Band {
+namespace Test {
+
+struct AffinityMasksFixture : public testing::TestWithParam<BandCPUMaskFlags> {
+};
+
+TEST_P(AffinityMasksFixture, AffinitySetTest) {
+  CpuSet target_set = BandCPUMaskGetSet(GetParam());
+  // this fails if target_set is null
+  BandStatus set_status = SetCPUThreadAffinity(target_set);
+
+  CpuSet current_set;
+  // should always success
+  EXPECT_EQ(GetCPUThreadAffinity(current_set), kBandOk);
+  if (set_status == kBandOk) {
+    EXPECT_EQ(target_set, current_set);
+    EXPECT_EQ(target_set.NumEnabled(), current_set.NumEnabled());
+  } else {
+    EXPECT_EQ(target_set.NumEnabled(), 0);
+  }
+}
+
+TEST(CPUTest, DisableTest) {
+  CpuSet set = BandCPUMaskGetSet(kBandAll);
+  EXPECT_EQ(SetCPUThreadAffinity(set), kBandOk);
+
+  for (size_t i = 0; i < GetCPUCount(); i++) {
+    set.Disable(i);
+  }
+
+  EXPECT_EQ(SetCPUThreadAffinity(set), kBandError);
+}
+
+TEST(CPUTest, EnableTest) {
+  CpuSet set;
+  EXPECT_EQ(SetCPUThreadAffinity(set), kBandError);
+
+  for (size_t i = 0; i < GetCPUCount(); i++) {
+    set.Enable(i);
+  }
+
+  EXPECT_EQ(SetCPUThreadAffinity(set), kBandOk);
+  EXPECT_EQ(GetCPUThreadAffinity(set), kBandOk);
+  EXPECT_EQ(set, BandCPUMaskGetSet(kBandAll));
+}
+
+INSTANTIATE_TEST_SUITE_P(AffinitySetTests, AffinityMasksFixture,
+                         testing::Values(kBandAll, kBandLittle, kBandBig,
+                                         kBandPrimary));
+
+}  // namespace Test
+}  // namespace Band
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
