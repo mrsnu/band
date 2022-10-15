b49ef98e92e34c506ff10a1098e938dc4d3f95c1
Jingyu Lee <dostos10@gmail.com>
2022-10-15 08:15:39 +0000

    undo change on sh

--- test_band.sh
+++ test_band.sh
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 bazel test --config tflite //band/...
 =======
 bazel test -c dbg --define tflite=true //band/...
 >>>>>>> 3baeae2e... Offline save / log + fname-based model matching
+=======
+bazel test --define tflite=true //band/...
+>>>>>>> 37878a67... undo change on sh
