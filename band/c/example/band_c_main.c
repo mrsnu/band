7e1e4964f931c94ff5c50b4cd6ebf936ecf518ed
Minjae Kim <widiba03304@gmail.com>
2022-10-19 15:56:12 +0900

    clang-format

--- band/c/example/band_c_main.c
+++ band/c/example/band_c_main.c
@@ -111,19 +111,19 @@ void LoadBandLibraryFunctions(void* libbandc) {
   LoadFunction(BandTypeGetName);
 }
 
-  bool LoadBandLibrary() {
+bool LoadBandLibrary() {
 #ifdef __WINDOWS__
-    HMODULE libbandc = LoadLibraryA("band_c.dll");
-    if (libbandc) {
-      LoadBandLibraryFunctions(libbandc);
-      return true;
-    } else {
-      DWORD error_code = GetLastError();
-      fprintf(stderr,
-              "Cannnot open Band C Library oon this device, error code - %d\n",
-              error_code);
-      return false;
-    }
+  HMODULE libbandc = LoadLibraryA("band_c.dll");
+  if (libbandc) {
+    LoadBandLibraryFunctions(libbandc);
+    return true;
+  } else {
+    DWORD error_code = GetLastError();
+    fprintf(stderr,
+            "Cannnot open Band C Library oon this device, error code - %d\n",
+            error_code);
+    return false;
+  }
 #else
   void* libbandc = NULL;
   libbandc = dlopen("libband_c.so", RTLD_NOW | RTLD_LOCAL);
@@ -132,93 +132,92 @@ void LoadBandLibraryFunctions(void* libbandc) {
     return true;
   }
 #endif
+}
+
+int main() {
+  bool success = LoadBandLibrary();
+  if (success) {
+    printf("Loaded!\n");
+  } else {
+    printf("Load failed.\n");
+    return -1;
+  }
+  BandConfigBuilder* b = pBandConfigBuilderCreate();
+  printf("BandConfigBuilder\n");
+  pBandAddConfig(b, BAND_PLANNER_LOG_PATH, /*count=*/1, "log.tsv");
+  printf("BandAddConfig, BAND_PLANNER_LOG_PATH\n");
+  pBandAddConfig(b, BAND_PLANNER_SCHEDULERS, /*count=*/1, kBandRoundRobin);
+  printf("BandAddConfig, BAND_PLANNER_SCHEDULERS\n");
+  pBandAddConfig(b, BAND_MINIMUM_SUBGRAPH_SIZE, /*count=*/1, 7);
+  printf("BandAddConfig, BAND_MINIMUM_SUBGRAPH_SIZE\n");
+  pBandAddConfig(b, BAND_SUBGRAPH_PREPARATION_TYPE, /*count=*/1,
+                 kBandMergeUnitSubgraph);
+  printf("BandAddConfig, BAND_SUBGRAPH_PREPARATION_TYPE\n");
+  pBandAddConfig(b, BAND_CPU_MASK, /*count=*/1, kBandAll);
+  printf("BandAddConfig, BAND_CPU_MASK\n");
+  pBandAddConfig(b, BAND_PLANNER_CPU_MASK, /*count=*/1, kBandPrimary);
+  printf("BandAddConfig, BAND_PLANNER_CPU_MASK\n");
+  pBandAddConfig(b, BAND_WORKER_WORKERS, /*count=*/2, kBandCPU, kBandCPU);
+  printf("BandAddConfig, BAND_WORKER_WORKERS\n");
+  pBandAddConfig(b, BAND_WORKER_NUM_THREADS, /*count=*/2, 3, 4);
+  printf("BandAddConfig, BAND_WORKER_NUM_THREADS\n");
+  pBandAddConfig(b, BAND_WORKER_CPU_MASKS, /*count=*/2, kBandBig, kBandLittle);
+  printf("BandAddConfig, BAND_WORKER_CPU_MASKS\n");
+  pBandAddConfig(b, BAND_PROFILE_SMOOTHING_FACTOR, /*count=*/1, 0.1f);
+  printf("BandAddConfig, BAND_PROFILE_SMOOTHING_FACTOR\n");
+  pBandAddConfig(b, BAND_PROFILE_DATA_PATH, /*count=*/1,
+                 "band/testdata/profile.json");
+  printf("BandAddConfig, BAND_PROFILE_DATA_PATH\n");
+  pBandAddConfig(b, BAND_PROFILE_ONLINE, /*count=*/1, true);
+  printf("BandAddConfig, BAND_PROFILE_ONLINE\n");
+  pBandAddConfig(b, BAND_PROFILE_NUM_WARMUPS, /*count=*/1, 1);
+  printf("BandAddConfig, BAND_PROFILE_NUM_WARMUPS\n");
+  pBandAddConfig(b, BAND_PROFILE_NUM_RUNS, /*count=*/1, 1);
+  printf("BandAddConfig, BAND_PROFILE_NUM_RUNS\n");
+  pBandAddConfig(b, BAND_WORKER_ALLOW_WORKSTEAL, /*count=*/1, true);
+  printf("BandAddConfig, BAND_WORKER_ALLOW_WORKSTEAL\n");
+  pBandAddConfig(b, BAND_WORKER_AVAILABILITY_CHECK_INTERVAL_MS, /*count=*/1,
+                 30000);
+  printf("BandAddConfig, BAND_WORKER_AVAILABILITY_CHECK_INTERVAL_MS\n");
+  pBandAddConfig(b, BAND_PLANNER_SCHEDULE_WINDOW_SIZE, /*count=*/1, 10);
+  printf("BandAddConfig, BAND_PLANNER_SCHEDULE_WINDOW_SIZE\n");
+  BandConfig* config = pBandConfigCreate(b);
+  printf("BandConfigCreate\n");
+
+  BandModel* model = pBandModelCreate();
+  printf("BandModelCreate\n");
+  pBandModelAddFromFile(model, kBandTfLite, "band/testdata/add.bin");
+  printf("BandModelAddFromFile\n");
+
+  BandEngine* engine = pBandEngineCreate(config);
+  printf("BandEngineCreate\n");
+  pBandEngineRegisterModel(engine, model);
+  printf("BandEngineRegisterModel\n");
+  pBandEngineGetNumInputTensors(engine, model);
+  printf("BandGetNumInputTensors\n");
+  pBandEngineGetNumOutputTensors(engine, model);
+  printf("BandGetNumOutputTensors\n");
+
+  BandTensor* input_tensor = pBandEngineCreateInputTensor(engine, model, 0);
+  printf("BandEngineCreateInputTensor\n");
+  BandTensor* output_tensor = pBandEngineCreateOutputTensor(engine, model, 0);
+  printf("BandEngineCreateOutputTensor\n");
+
+  float input[] = {1.f, 3.f};
+  memcpy(pBandTensorGetData(input_tensor), input, 2 * sizeof(float));
+  printf("BandTensorGetData\n");
+  pBandEngineRequestSync(engine, model, &input_tensor, &output_tensor);
+  printf("BandEngineRequestSync\n");
+
+  if (((float*)pBandTensorGetData(output_tensor))[0] == 3.f &&
+      ((float*)pBandTensorGetData(output_tensor))[1] == 9.f) {
+    printf("Success!\n");
   }
 
-  int main() {
-    bool success = LoadBandLibrary();
-    if (success) {
-      printf("Loaded!\n");
-    } else {
-      printf("Load failed.\n");
-      return -1;
-    }
-    BandConfigBuilder* b = pBandConfigBuilderCreate();
-    printf("BandConfigBuilder\n");
-    pBandAddConfig(b, BAND_PLANNER_LOG_PATH, /*count=*/1, "log.tsv");
-    printf("BandAddConfig, BAND_PLANNER_LOG_PATH\n");
-    pBandAddConfig(b, BAND_PLANNER_SCHEDULERS, /*count=*/1, kBandRoundRobin);
-    printf("BandAddConfig, BAND_PLANNER_SCHEDULERS\n");
-    pBandAddConfig(b, BAND_MINIMUM_SUBGRAPH_SIZE, /*count=*/1, 7);
-    printf("BandAddConfig, BAND_MINIMUM_SUBGRAPH_SIZE\n");
-    pBandAddConfig(b, BAND_SUBGRAPH_PREPARATION_TYPE, /*count=*/1,
-                   kBandMergeUnitSubgraph);
-    printf("BandAddConfig, BAND_SUBGRAPH_PREPARATION_TYPE\n");
-    pBandAddConfig(b, BAND_CPU_MASK, /*count=*/1, kBandAll);
-    printf("BandAddConfig, BAND_CPU_MASK\n");
-    pBandAddConfig(b, BAND_PLANNER_CPU_MASK, /*count=*/1, kBandPrimary);
-    printf("BandAddConfig, BAND_PLANNER_CPU_MASK\n");
-    pBandAddConfig(b, BAND_WORKER_WORKERS, /*count=*/2, kBandCPU, kBandCPU);
-    printf("BandAddConfig, BAND_WORKER_WORKERS\n");
-    pBandAddConfig(b, BAND_WORKER_NUM_THREADS, /*count=*/2, 3, 4);
-    printf("BandAddConfig, BAND_WORKER_NUM_THREADS\n");
-    pBandAddConfig(b, BAND_WORKER_CPU_MASKS, /*count=*/2, kBandBig,
-                   kBandLittle);
-    printf("BandAddConfig, BAND_WORKER_CPU_MASKS\n");
-    pBandAddConfig(b, BAND_PROFILE_SMOOTHING_FACTOR, /*count=*/1, 0.1f);
-    printf("BandAddConfig, BAND_PROFILE_SMOOTHING_FACTOR\n");
-    pBandAddConfig(b, BAND_PROFILE_DATA_PATH, /*count=*/1,
-                   "band/testdata/profile.json");
-    printf("BandAddConfig, BAND_PROFILE_DATA_PATH\n");
-    pBandAddConfig(b, BAND_PROFILE_ONLINE, /*count=*/1, true);
-    printf("BandAddConfig, BAND_PROFILE_ONLINE\n");
-    pBandAddConfig(b, BAND_PROFILE_NUM_WARMUPS, /*count=*/1, 1);
-    printf("BandAddConfig, BAND_PROFILE_NUM_WARMUPS\n");
-    pBandAddConfig(b, BAND_PROFILE_NUM_RUNS, /*count=*/1, 1);
-    printf("BandAddConfig, BAND_PROFILE_NUM_RUNS\n");
-    pBandAddConfig(b, BAND_WORKER_ALLOW_WORKSTEAL, /*count=*/1, true);
-    printf("BandAddConfig, BAND_WORKER_ALLOW_WORKSTEAL\n");
-    pBandAddConfig(b, BAND_WORKER_AVAILABILITY_CHECK_INTERVAL_MS, /*count=*/1,
-                   30000);
-    printf("BandAddConfig, BAND_WORKER_AVAILABILITY_CHECK_INTERVAL_MS\n");
-    pBandAddConfig(b, BAND_PLANNER_SCHEDULE_WINDOW_SIZE, /*count=*/1, 10);
-    printf("BandAddConfig, BAND_PLANNER_SCHEDULE_WINDOW_SIZE\n");
-    BandConfig* config = pBandConfigCreate(b);
-    printf("BandConfigCreate\n");
-
-    BandModel* model = pBandModelCreate();
-    printf("BandModelCreate\n");
-    pBandModelAddFromFile(model, kBandTfLite, "band/testdata/add.bin");
-    printf("BandModelAddFromFile\n");
-
-    BandEngine* engine = pBandEngineCreate(config);
-    printf("BandEngineCreate\n");
-    pBandEngineRegisterModel(engine, model);
-    printf("BandEngineRegisterModel\n");
-    pBandEngineGetNumInputTensors(engine, model);
-    printf("BandGetNumInputTensors\n");
-    pBandEngineGetNumOutputTensors(engine, model);
-    printf("BandGetNumOutputTensors\n");
-
-    BandTensor* input_tensor = pBandEngineCreateInputTensor(engine, model, 0);
-    printf("BandEngineCreateInputTensor\n");
-    BandTensor* output_tensor = pBandEngineCreateOutputTensor(engine, model, 0);
-    printf("BandEngineCreateOutputTensor\n");
-
-    float input[] = {1.f, 3.f};
-    memcpy(pBandTensorGetData(input_tensor), input, 2 * sizeof(float));
-    printf("BandTensorGetData\n");
-    pBandEngineRequestSync(engine, model, &input_tensor, &output_tensor);
-    printf("BandEngineRequestSync\n");
-
-    if (((float*)pBandTensorGetData(output_tensor))[0] == 3.f &&
-        ((float*)pBandTensorGetData(output_tensor))[1] == 9.f) {
-      printf("Success!\n");
-    }
-
-    pBandEngineDelete(engine);
-    pBandTensorDelete(input_tensor);
-    pBandTensorDelete(output_tensor);
-    pBandConfigDelete(config);
-    pBandModelDelete(model);
-    return 0;
-  }
+  pBandEngineDelete(engine);
+  pBandTensorDelete(input_tensor);
+  pBandTensorDelete(output_tensor);
+  pBandConfigDelete(config);
+  pBandModelDelete(model);
+  return 0;
+}
