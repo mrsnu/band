0123d5b722b644f4ee1719886a18d701bb71c997
Minjae Kim <widiba03304@gmail.com>
2023-01-17 18:15:54 +0900

    xnnpack ignore

--- script/build_aar_armv8.sh
+++ script/build_aar_armv8.sh
@@ -1,4 +1,4 @@
 rm -f band.aar
-bazel build -c dbg --config android_arm64_tflite //band/java:band
+bazel build -c dbg --config android_arm64_tflite --define tflite_with_xnnpack=false //band/java:band
 cp bazel-bin/band/java/band.aar ./
 
