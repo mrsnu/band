74fbc213a5686a551167fbf4a3ef0c9e3ed1e1a0
Jingyu Lee <dostos10@gmail.com>
2023-03-27 16:55:08 +0900

    fix build scripts for windows

--- script/utils/util.py
+++ script/utils/util.py
@@ -35,7 +35,7 @@ def canon_path(path):
     if is_windows() or is_cygwin():
         return path.replace('\\', '/')
     return path
-        
+
 
 def run_cmd(cmd):
     args = shlex.split(cmd)
@@ -63,23 +63,24 @@ def get_bazel_options(
     opt = ""
     opt += "-c " + ("dbg" if debug else "opt") + " "
     opt += "--strip " + ("never" if debug else "always") + " "
-    opt += "--config " + f"{PLATFORM[platform]}" + ("" if backend == "none" else f"_{backend}") + " "
+    opt += "--config " + f"{PLATFORM[platform]}" + \
+        ("" if backend == "none" else f"_{backend}") + " "
     return opt
 
 
 def make_cmd(
-        build_only: bool, 
-        debug: bool, 
-        platform: str, 
-        backend: str, 
-        target: str,
-    ):
+    build_only: bool,
+    debug: bool,
+    platform: str,
+    backend: str,
+    target: str,
+):
     cmd = "bazel" + " "
     cmd += ("build" if build_only else "test") + " "
     cmd += get_bazel_options(debug, platform, backend)
     cmd += target
     return cmd
-    
+
 
 def get_dst_path(base_dir, target_platform, debug):
     build = 'debug' if debug else 'release'
@@ -100,14 +101,17 @@ def run_binary_android(basepath, path, option=''):
     run_cmd(
         f'adb -d shell cd {ANDROID_BASE}{basepath} && ./{path} {option}')
 
+
 def get_argument_parser(desc: str):
     parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('-B', '--backend', type=str, default='tflite', help='Backend <tflite|none>')
+    parser.add_argument('-B', '--backend', type=str,
+                        default='tflite', help='Backend <tflite|none>')
     parser.add_argument('-android', action="store_true", default=False,
                         help='Test on Android (with adb)')
     parser.add_argument('-docker', action="store_true", default=False,
                         help='Compile / Pull cross-compiled binaries for android from docker (assuming that the current docker context has devcontainer built with a /.devcontainer')
-    parser.add_argument('-s', '--ssh', required=False, default=None, help="SSH host name (e.g. dev@ssh.band.org)")
+    parser.add_argument('-s', '--ssh', required=False, default=None,
+                        help="SSH host name (e.g. dev@ssh.band.org)")
     parser.add_argument('-d', '--debug', action="store_true", default=False,
                         help='Build debug (default = release)')
     parser.add_argument('-r', '--rebuild', action="store_true", default=False,
@@ -115,6 +119,7 @@ def get_argument_parser(desc: str):
     parser.add_argument('-b', '--build', action="store_true", default=False,
                         help='Build only, only affects to local (default = false)')
     return parser
+
 
 def clean_bazel(docker):
     if docker:
