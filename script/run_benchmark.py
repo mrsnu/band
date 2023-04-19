45af800887b0bd530e84d15324b010f201e7067e
Minjae Kim <widiba03304@gmail.com>
2023-04-19 16:37:47 +0900

    fix benchmark script

--- script/run_benchmark.py
+++ script/run_benchmark.py
@@ -31,7 +31,12 @@ def benchmark_local(debug, trace, platform, backend, build_only, config_path):
             f'{BASE_DIR}/band_benchmark.exe {config_path}'
         )
 
-def benchmark_android(debug, trace, platform, backend, docker, config_path=""):
+def benchmark_android(
+        debug, 
+        trace,  
+        backend, 
+        docker, 
+        config_path=""):
     target_base_dir = BASE_DIR
     # build android targets only (specified in band_cc_android_test tags)
     
@@ -47,12 +52,10 @@ def benchmark_android(debug, trace, platform, backend, docker, config_path=""):
         run_cmd_docker(build_command)
         # create a local path
         subprocess.call(['mkdir', '-p', f'{target_base_dir}'])
-        # run_cmd(
-        #     f'sh script/docker_util.sh -d bazel-bin/band/tool/band_benchmark {target_base_dir}')
         copy_docker('bazel-bin/band/tool/band_benchmark', target_base_dir)
-    elif platform.system() == 'Linux':
-        run_cmd(build_command)
-        copy('bazel-bin/band/tool/band_benchmark', target_base_dir)
+    
+    run_cmd(build_command)
+    copy('bazel-bin/band/tool/band_benchmark', target_base_dir)
 
     shutil.copy(config_path, f'{target_base_dir}/config.json')
     push_to_android(f'{target_base_dir}', '')
@@ -77,7 +80,7 @@ if __name__ == '__main__':
     if args.android:
         # Need to set Android build option in ./configure
         print('Benchmark Android')
-        benchmark_android(args.debug, args.trace, get_platform(), args.backend,
+        benchmark_android(args.debug, args.trace, args.backend,
                           args.docker, args.config)
     else:
         print(f'Benchmark {get_platform()}')
