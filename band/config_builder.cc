5b83a2a5dd0e8207acf9bf9122964690aadd217c
Minjae Kim <widiba03304@gmail.com>
2023-08-28 04:31:46 +0000

    fix propagation to subgraph config

--- band/config_builder.cc
+++ band/config_builder.cc
@@ -150,6 +150,8 @@ absl::StatusOr<RuntimeConfig> RuntimeConfigBuilder::Build() {
   runtime_config.worker_config = worker_config_builder_.Build().value();
   runtime_config.resource_monitor_config =
       resource_monitor_config_builder_.Build().value();
+  runtime_config.subgraph_config = {minimum_subgraph_size_,
+                                    subgraph_preparation_type_};
   return runtime_config;
 }
 }  // namespace band
