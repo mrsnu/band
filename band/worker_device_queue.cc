190a2b5e7b50037ef16ba1bd67d51c2361c79533
Jingyu Lee <dostos10@gmail.com>
2023-10-05 00:57:59 +0000

    minor: remove verbose log from device queue

--- band/worker_device_queue.cc
+++ band/worker_device_queue.cc
@@ -64,10 +64,6 @@ bool DeviceQueueWorker::EnqueueJob(Job& job) {
   if (!IsEnqueueReady()) {
     return false;
   }
-
-  BAND_LOG_PROD(BAND_LOG_INFO, "Enqueue job %d to worker %d", job.job_id,
-                worker_id_);
-
   requests_.push_back(job);
   request_cv_.notify_one();
   return true;
